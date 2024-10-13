1eb3d72366f3bd4793bf6fcd095b9d21faefaeea
SravanKumar Polu <99873642+SravanKumarPolu@users.noreply.github.com>
2024-10-13 23:41:50 +0530

    svelte practice project

--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,53 @@
 <script>
-let name = " Svelte Demo";
-let point=100;
+	
+	import Navbar from "./Navbar.svelte";
+import Player from "./Player.svelte";
+import AddPlayer from "./AddPlayer.svelte";
+let players=[
+	{
+		name:"Madhu",
+		points:37
+
+	},
+	{
+		name:"Narasimha",
+		points:30
+
+	},
+	{
+		name:"SravanKumar",
+		points:30
+
+	},
+	{
+		name:"Venky",
+		points:24
+
+	},
+];
+const addPlayer=(e)=>{
+	const newPlayer=e.detail;
+	players=[...players, newPlayer]
+}
+const removePlayer=e=>{
+	players=players.filter(player=>player.name !==  e.detail);
+};
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<Navbar />
+	<div class="container">
+		<AddPlayer on:addplayer={addPlayer} />
+     {#if players.length===0}
+		 <p>No Players</p>
+		 {:else}
+        {#each players as player}
+				<Player  name={player.name} points={player.points} on:removeplayer={removePlayer}/>
+				{/each}
+		 {/if}
+	</div>
+
 </main>
+ 
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
---
+++ src/Player.svelte
@@ -0,0 +1,42 @@
+<script>
+  import { createEventDispatcher} from "svelte"
+  const dispatch=createEventDispatcher();
+export let name;
+export let points;
+let showControls=false;
+const addPoint=()=>(points +=1);
+const removePoint=()=>(points-=1);
+const toggleControls=()=>(showControls = !showControls)
+const onDelete=()=>dispatch("removeplayer",name);
+</script>
+
+
+<div class="card">
+
+	<h1> 
+		{name.toUpperCase()}
+	<button class="btn btn-sm" on:click={toggleControls}>
+		{#if showControls}-{:else}+{/if} 
+	</button>
+  <button class="btn btn-dander btn-sm" on:click={onDelete}>X</button>
+	</h1>
+	<p>Points:{points}</p>
+   {#if showControls}
+	<button class="btn" on:click={addPoint}>+1</button>
+	<button class="btn btn-dark" on:click={removePoint}>-1</button>
+	 <input type="number" bind:value={points} />
+	  {/if}
+</div>
+
+ 
+<style>
+
+
+	h1 {
+		color: #ff3e00;
+	
+	}
+p{
+	margin-bottom: 10px;
+}
+</style>
